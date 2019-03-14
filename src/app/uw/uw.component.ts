9b40a71c447179481007c205ae780303cf97e1eb
sanjaynegi <sanjaynegi24jul@gmail.com>
2019-03-14 23:47:44 +0530

    uw update

--- src/app/uw/uw.component.ts
+++ src/app/uw/uw.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import {animate, AnimationOptions} from '@angular/animations';
+
 
 @Component({
   selector: 'app-uw',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UwComponent implements OnInit {
 
+  myDoughnutChart=[];
+
+   
+data = {
+  datasets: [{
+      data: [45, 55],
+      backgroundColor: [
+        'rgba(229, 0, 0)',
+        'rgba(214, 212, 209)'
+    ]
+  }],
+
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: [
+      // 'Red',
+      // 'Yellow',
+      // 'Blue'
+  ]
+};
+
   constructor() { }
 
   ngOnInit() {
+    this.myDoughnutChart = new Chart('canvas', {
+      type: 'doughnut',
+      data: this.data,
+      options: {cutoutPercentage:80,
+        circumference:Math.PI,
+        rotation : -Math.PI
+        }
+      
+  });
   }
 
 }
