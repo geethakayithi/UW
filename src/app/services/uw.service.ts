b0b757a2ee5085d00fea47ca1620c5b7f5a4d415
sanjaynegi <sanjaynegi24jul@gmail.com>
2019-03-17 18:11:16 +0530

    update

--- src/app/services/uw.service.ts
+++ src/app/services/uw.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UwService {
+  private header = new HttpHeaders()
+    .set('Content-Type', 'application/son');
 
   constructor(private http: HttpClient) { }
   url = 'https://my-json-server.typicode.com/sanjaynegi/jsonTestRepo/Provenir'
 
   getUWdata(uwReqObject) {
-    // return this.http.get(this.url,uwReqObject );
-    return this.http.post(this.url,uwReqObject );
+    return this.http.get(this.url,uwReqObject );
+    // return this.http.post(this.url,uwReqObject, {headers: this.header} );
   }
 }
--- src/app/uw/uw.component.ts
+++ src/app/uw/uw.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Chart } from 'chart.js';
-import {animate, AnimationOptions} from '@angular/animations';
+import { animate, AnimationOptions } from '@angular/animations';
 import { UwService } from '../services/uw.service';
 
 
@@ -12,59 +13,111 @@ import { UwService } from '../services/uw.service';
 })
 export class UwComponent implements OnInit {
 
-  myDoughnutChart=[];
-  uwObject={};
-  uwReqObject={  
-    "Provenir":  
-    {  
-        "Request":  
-        {  
-            "ID": "138867",  
-            "UserID": "43932172"  
-        }  
-    }  
-};
-
+  appScoreChart = [];
+  busCreditChart =[];
+  consumerCreditChart =[];
+  uwObject = {};
+  uwReqObject = {
+    "Provenir":
+    {
+      "Request":
+      {
+        "ID": "138867",
+        "UserID": "43932172"
+      }
+    }
+  };
   
 
-
-   
-data = {
-  datasets: [{
-      data: [45, 55],
-      backgroundColor: [
-        'rgba(229, 0, 0)',
-        'rgba(214, 212, 209)'
-    ]
-  }],
-
-  // These labels appear in the legend and in the tooltips when hovering different arcs
-  labels: [
-      // 'Red',
-      // 'Yellow',
-      // 'Blue'
-  ]
-};
-
-  constructor(private uwService: UwService) { }
+  constructor(private uwService: UwService, private elementRef: ElementRef) { }
 
   ngOnInit() {
     this.getUWdata();
-    this.myDoughnutChart = new Chart('canvas', {
-      type: 'doughnut',
-      data: this.data,
-      options: {cutoutPercentage:80,
-        circumference:Math.PI,
-        rotation : -Math.PI
-        }
-      
-  });
+
   }
-  getUWdata(){
+
+  getUWdata() {
     this.uwService.getUWdata(this.uwReqObject)
-    .subscribe((data) =>  {
-      this.uwObject=data;
-      console.log(this.uwObject);
+      .subscribe((data) => {
+        this.uwObject = data;
+        console.log(this.uwObject);
+        this.renderAppScoreChart();
+        this.renderBussCreditScoreChart();
+        this.renderConsCreditScoreChart();
+      });
+  }
+  renderAppScoreChart() {
+    var ctx = document.getElementById("appscorechart");
+    let data = {
+      datasets: [{
+        data: [45, 55],
+        backgroundColor: [
+          'rgba(229, 0, 0)',
+          'rgba(214, 212, 209)'
+        ]
+      }],
+      labels: [
+        // 'Red'
+        ]
+    };
+    this.appScoreChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: {
+        cutoutPercentage: 80,
+        circumference: Math.PI,
+        rotation: -Math.PI
+      }
+    });
+  }
+  renderBussCreditScoreChart() {
+    var ctx = document.getElementById("buscreditcorechart");
+    let data = {
+      datasets: [{
+        data: [45, 55],
+        backgroundColor: [
+          'rgba(229, 0, 0)',
+          'rgba(214, 212, 209)'
+        ]
+      }],
+      labels: [
+        // 'Red'
+  
+      ]
+    };
+    this.busCreditChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: {
+        cutoutPercentage: 80,
+        circumference: Math.PI,
+        rotation: -Math.PI
+      }
+    });
+  }
+  renderConsCreditScoreChart() {
+    var ctx = document.getElementById("conscreditcorechart");
+    let data = {
+      datasets: [{
+        data: [45, 55],
+        backgroundColor: [
+          'rgba(229, 0, 0)',
+          'rgba(214, 212, 209)'
+        ]
+      }],
+      labels: [
+        // 'Red'
+  
+      ]
+    };
+    this.consumerCreditChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: {
+        cutoutPercentage: 80,
+        circumference: Math.PI,
+        rotation: -Math.PI
+      }
     });
   }
 
